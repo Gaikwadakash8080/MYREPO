f2c5c3c94d5a203a6f0383bc4cba6d950c33f8e8
= <=>
2023-06-22 12:39:38 +0530

    first Commit

--- src/sourceCode/CC.java
+++ src/sourceCode/CC.java
@@ -8,10 +8,23 @@ public class CC {
 	public static void main(String[] args) {
 		
 		
+	Integer number = 100;
+	int n = 200;
+	String user = "Animal";
+	String password = "Monkey";
 	
+	
+	String num = number.toString();
+	String nn = Integer.toString(n);
+	
+	// Concatenating above four variable. after converting integers to string.
+	// which was previously not possible
+	
+	System.out.println(user+num);
+	System.out.println(password+nn);
 
 
-		}
+		
 
 		
 	}
